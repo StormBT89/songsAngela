a1d800ebaf28dc5bd0dcc282fdb92726d7720821
StormBT89 <glavevski.pece@gmail.com>
2024-10-31 22:32:10 +0100

    Deploy website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
+import {useState} from "react";
+
+
 
 function App() {
+  const [artist, setArtist] = useState("");
+  const [song, setSong] = useState("");
+  const [lyrics, setLyrics] = useState("");
+  const url = "https://api.lyrics.ovh/v1/" + artist + "/" + song + "/"
+  
+
+  function searchLyrics() {
+    console.log(url);
+    console.log(artist);
+    console.log(song);
+
+    if (artist === "" || song === "") {
+      return;
+    } 
+     Axios.get(url).then(res => {
+      setLyrics(res.data.lyrics);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Апликацијата на Ангела за избор на текст од песна</h1>
+
+      <input className="inp" type="text" placeholder='Име на изведувач'
+      onChange={(e) => {
+        setArtist(e.target.value)
+        console.log(artist)}}/>
+       <input className="inp" type="text" placeholder='Име на песната'
+      onChange={(e) => {
+        setSong(e.target.value)
+        console.log(song)}}/>
+        <button className="btn" onClick={() => searchLyrics()}>Пребарaj</button>
+        <hr/>
+
+        <pre>{lyrics}</pre>      
     </div>
   );
 }
